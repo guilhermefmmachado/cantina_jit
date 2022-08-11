c39e40e313a607283898d700b16bc3a37dd57925
guilhermefmmachado <guilherme.fmachado@outlook.com>
2022-08-10 21:32:18 -0300

    Cabeçalho do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cantina_jit/navigations/tabbar.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,3 +1,4 @@
+import 'package:cantina_jit/auxiliar-classes/app_color_palette.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -11,8 +12,63 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Image.asset("assets/system_images/home-wallpaper.jpg"),
-    ));
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Image(
+                  image: AssetImage("assets/system_images/home-wallpaper.jpg"),
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.cover,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: AppColorPalette.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          spreadRadius: 2,
+                          blurRadius: 6,
+                        ),
+                      ]),
+                  width: MediaQuery.of(context).size.width,
+                  height: 200,
+                  margin: EdgeInsets.fromLTRB(20, 100, 20, 0),
+                  child: Row(
+                    children: [
+                      Image(
+                          image: AssetImage(
+                              "assets/system_images/logos/Cantina JIT-logos_transparent.png")),
+                      Expanded(
+                        child: Text(
+                          "Bem-vindo à Cantina JIT!",
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Text(
+                    "O que lhe traz aqui hoje?",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                ),
+              ],
+            ),
+            // ROWS E GESTURE DETECTOR
+          ],
+        ),
+      ),
+    );
   }
 }
