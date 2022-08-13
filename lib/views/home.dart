ddf1fd7dc23cc7ddd63e2ae4bc30457d011a4a56
guilhermefmmachado <guilherme.fmachado@outlook.com>
2022-08-13 12:05:21 -0300

    Criando widget para o cabeçalho da home.

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:cantina_jit/auxiliar-classes/app_color_palette.dart';
+import 'package:cantina_jit/widgets/home_header.dart';
 import 'package:cantina_jit/widgets/home_nav_button.dart';
 import 'package:flutter/material.dart';
 
@@ -16,45 +17,7 @@ class _HomeViewState extends State<HomeView> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Stack(
-              children: [
-                Image(
-                  image: AssetImage("assets/system_images/home-wallpaper.jpg"),
-                  width: MediaQuery.of(context).size.width,
-                  fit: BoxFit.cover,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: AppColorPalette.white,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.2),
-                          spreadRadius: 2,
-                          blurRadius: 6,
-                        ),
-                      ]),
-                  width: MediaQuery.of(context).size.width,
-                  height: 200,
-                  margin: EdgeInsets.fromLTRB(20, 100, 20, 0),
-                  child: Row(
-                    children: [
-                      Image(
-                          image: AssetImage(
-                              "assets/system_images/logos/Cantina JIT-logos_transparent.png")),
-                      Expanded(
-                        child: Text(
-                          "Bem-vindo à Cantina JIT!",
-                          style: TextStyle(
-                            fontSize: 24,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+            HomeHeader(),
             Row(
               children: [
                 Padding(
