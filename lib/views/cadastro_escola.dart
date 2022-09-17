b2e53b568110c120a53ccf7a70c2e58ac48dc361
guilhermefmmachado <guilherme.fmachado@outlook.com>
2022-09-17 19:45:16 -0300

    Removendo importações inúteis.

--- lib/views/cadastro_escola.dart
+++ lib/views/cadastro_escola.dart
@@ -1,7 +1,5 @@
 import 'package:cantina_jit/auxiliar-classes/app_color_palette.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class CadastroEscolaView extends StatefulWidget {
   const CadastroEscolaView({Key? key}) : super(key: key);
