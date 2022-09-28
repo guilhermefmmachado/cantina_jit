ab65d9e4aabdaeb33dd7a2164c5d613d4d8f273a
guilhermefmmachado <guilherme.fmachado@outlook.com>
2022-09-28 10:58:22 -0300

    Início da comunicação com o servidor (a concluir)

--- lib/views/editar_cardapio_views/adicionar_produto.dart
+++ lib/views/editar_cardapio_views/adicionar_produto.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:cantina_jit/auxiliar-classes/app_color_palette.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class AdicionarProdutoView extends StatefulWidget {
   const AdicionarProdutoView({Key? key}) : super(key: key);
@@ -11,7 +14,51 @@ class AdicionarProdutoView extends StatefulWidget {
 // TODO: Estilizar os campos.
 // ! Atenção aos retornos NULL
 class _AdicionarProdutoViewState extends State<AdicionarProdutoView> {
+  TextEditingController nomeProdutoCtl = TextEditingController();
+  TextEditingController descProdutoCtl = TextEditingController();
+  TextEditingController tipoProdutoCtl = TextEditingController();
+  TextEditingController precoProdutoCtl = TextEditingController();
+  TextEditingController estoqueProdutoCtl = TextEditingController();
+
+  //text controller for TextField
+
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  late bool erro, enviando, sucesso;
+  late String msg;
+
+  String phpUrl =
+      "http://179.183.164.113/projetos_flutter/cantina_jit_backend/index.php";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    erro = false;
+    enviando = false;
+    sucesso = false;
+    msg = "";
+    super.initState();
+  }
+
+  Future<void> addProduto() async {
+    var response = await http.post(Uri.parse(phpUrl), body: {
+      // $novoProduto = new Produto($_POST["nome"], $_POST["desc"], $_POST["tipo"], $_POST["preco"], $_POST["estoque"]);
+      "nome": nomeProdutoCtl.text,
+      "desc": descProdutoCtl.text,
+      "tipo": tipoProdutoCtl.text,
+      "preco": precoProdutoCtl.text,
+      "estoque": estoqueProdutoCtl.text,
+    });
+
+    if (response.statusCode == 200) {
+      print(response.body);
+      // ! Rever o que "data" fala
+      var data = json.decode(response.body);
+      if (data["erro"]) {
+        // TODO: Completar o código daqui
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +76,7 @@ class _AdicionarProdutoViewState extends State<AdicionarProdutoView> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 TextFormField(
+                  controller: nomeProdutoCtl,
                   decoration: const InputDecoration(
                     hintText: "Nome do produto",
                     focusedBorder: UnderlineInputBorder(
@@ -46,6 +94,7 @@ class _AdicionarProdutoViewState extends State<AdicionarProdutoView> {
                   },
                 ),
                 TextFormField(
+                  controller: descProdutoCtl,
                   decoration: const InputDecoration(
                     hintText: "Descrição do produto (opcional)",
                     focusedBorder: UnderlineInputBorder(
@@ -57,6 +106,7 @@ class _AdicionarProdutoViewState extends State<AdicionarProdutoView> {
                   ),
                 ),
                 TextFormField(
+                  controller: tipoProdutoCtl,
                   decoration: const InputDecoration(
                     hintText: "Tipo ou categoria do produto",
                     focusedBorder: UnderlineInputBorder(
@@ -74,6 +124,7 @@ class _AdicionarProdutoViewState extends State<AdicionarProdutoView> {
                   },
                 ),
                 TextFormField(
+                  controller: precoProdutoCtl,
                   decoration: const InputDecoration(
                     hintText: "Preço do produto (R\$)",
                     focusedBorder: UnderlineInputBorder(
@@ -93,6 +144,7 @@ class _AdicionarProdutoViewState extends State<AdicionarProdutoView> {
                   },
                 ),
                 TextFormField(
+                    controller: estoqueProdutoCtl,
                     decoration: const InputDecoration(
                       hintText: "Estoque",
                       focusedBorder: UnderlineInputBorder(
