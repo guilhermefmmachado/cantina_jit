55abdebc5ea013dd0db5848026935b216c2204f0
guilhermefmmachado <guilherme.fmachado@outlook.com>
2022-10-05 20:48:00 -0300

    Início do desenvolvimento dos cadastros dos atores

--- lib/views/cadastro/is_atores.dart
+++ lib/views/cadastro/is_atores.dart
@@ -1,3 +1,4 @@
+import 'package:cantina_jit/auxiliar-classes/app_color_palette.dart';
 import 'package:flutter/material.dart';
 
 class IsClienteFuncionarioGerenteView extends StatelessWidget {
@@ -8,13 +9,74 @@ class IsClienteFuncionarioGerenteView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Iniciando cadastro..."),
+        backgroundColor: AppColorPalette.redMain,
       ),
       body: Column(
         children: [
-          Center(
-            child: Text("Informe-nos como você pretende usar nosso app"),
+          SizedBox(
+            height: 24,
           ),
-          
+          Center(
+            child: Text("Informe-nos quem é você ao usar nosso app."),
+          ),
+          SizedBox(height: 24),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+            child: ElevatedButton(
+              onPressed: () {
+                print("Teste...");
+              },
+              style: ElevatedButton.styleFrom(
+                primary: AppColorPalette.redMain,
+              ),
+              child: Row(
+                children: [
+                  SizedBox(width: 24),
+                  Icon(Icons.person_outline_rounded),
+                  SizedBox(width: 8),
+                  Text("Cliente"),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+            child: ElevatedButton(
+              onPressed: () {
+                print("Teste...");
+              },
+              style: ElevatedButton.styleFrom(
+                primary: AppColorPalette.redMain,
+              ),
+              child: Row(
+                children: [
+                  SizedBox(width: 24),
+                  Icon(Icons.room_service_outlined),
+                  SizedBox(width: 8),
+                  Text("Funcionário"),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+            child: ElevatedButton(
+              onPressed: () {
+                print("Teste...");
+              },
+              style: ElevatedButton.styleFrom(
+                primary: AppColorPalette.redMain,
+              ),
+              child: Row(
+                children: [
+                  SizedBox(width: 24),
+                  Icon(Icons.account_box_rounded),
+                  SizedBox(width: 8),
+                  Text("Gerente"),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
