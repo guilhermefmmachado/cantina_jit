3826825e0c9139e7783742635af570143b55c448
guilhermefmmachado <guilherme.fmachado@outlook.com>
2022-10-15 15:27:02 -0300

    feature login funcional

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
+import 'package:cantina_jit/views/login.dart';
 import 'package:flutter/material.dart';
 import 'package:cantina_jit/navigations/tabbar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // * Shared Preferences: banco de dados "cache" do celular.
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  var email = preferences.getString("email");
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: email == null ? LoginView() : Tabbar(),
+    ),
+  );
 }
 
+/*
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -24,3 +39,4 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+*/
--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:cantina_jit/auxiliar-classes/app_color_palette.dart';
+import 'package:cantina_jit/navigations/tabbar.dart';
 import 'package:cantina_jit/views/cadastro/is_atores.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -9,11 +14,80 @@ class LoginView extends StatefulWidget {
   State<LoginView> createState() => _LoginViewState();
 }
 
-// TODO: Estilizar os campos.
-// ! Atenção aos retornos NULL
 class _LoginViewState extends State<LoginView> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController emailCtl = TextEditingController();
+  TextEditingController senhaCtl = TextEditingController();
   bool _isPasswordFieldObscure = true;
+
+  final List<String> _atores = <String>["Cliente", "Funcionário", "Gerente"];
+  late String _valorAtoresDropdown;
+
+  late bool erro, enviando, sucesso;
+  late String msg;
+
+  String phpUrl =
+      "http://192.168.15.9/projetos_flutter/cantina_jit_backend/controllers/login_ctl.php";
+  // ! CUIDADO AO USAR NA ESCOLA, ERRO DE IP.
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    erro = false;
+    enviando = false;
+    sucesso = false;
+    msg = "";
+    _valorAtoresDropdown = _atores.first;
+    super.initState();
+  }
+
+  Future fazerAutenticacao() async {
+    var response = await http.post(Uri.parse(phpUrl), body: {
+      // $login = new Login($_POST["email"], $_POST["senha"], $_POST["ator"], $condicaoExecucao);
+      "email": emailCtl.text,
+      "senha": senhaCtl.text,
+      "ator": _valorAtoresDropdown,
+    });
+
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      if (data["erro"]) {
+        setState(() {
+          enviando = false;
+          erro = true;
+          msg = data["msg"];
+        });
+      } else {
+        setState(() {
+          enviando = false;
+          sucesso = true;
+        });
+
+        if (sucesso) {
+          SharedPreferences preferences = await SharedPreferences.getInstance();
+          preferences.setString("email", emailCtl.text);
+
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const Tabbar(),
+            ),
+          );
+        }
+
+        emailCtl.text = "";
+        senhaCtl.text = "";
+      }
+    } else {
+      setState(() {
+        erro = true;
+        msg = "Erro no envio do conteúdo. ${response.statusCode}";
+        enviando = false;
+      });
+    }
+
+    // ! FAZER PHP P/ REMOVER DEAD CODE
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,6 +122,8 @@ class _LoginViewState extends State<LoginView> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               TextFormField(
+                controller: emailCtl,
+                keyboardType: TextInputType.emailAddress,
                 decoration: const InputDecoration(
                   hintText: "usuario@example.com",
                   labelText: "E-mail do usuário",
@@ -70,6 +146,7 @@ class _LoginViewState extends State<LoginView> {
                 height: 32,
               ),
               TextFormField(
+                controller: senhaCtl,
                 obscureText: _isPasswordFieldObscure,
                 decoration: InputDecoration(
                   hintText: "Senha",
@@ -100,11 +177,33 @@ class _LoginViewState extends State<LoginView> {
               const SizedBox(
                 height: 32,
               ),
+              DropdownButton(
+                value: _valorAtoresDropdown,
+                icon: const Icon(Icons.keyboard_arrow_down_rounded),
+                elevation: 16,
+                underline: Container(
+                  height: 2,
+                  color: AppColorPalette.greenMain,
+                ),
+                items: _atores.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? value) {
+                  // Usado quando o usuário selecionar um item
+                  setState(() {
+                    _valorAtoresDropdown = value!;
+                  });
+                },
+              ),
               Center(
                 child: ElevatedButton(
                   onPressed: () {
                     if (_formKey.currentState!.validate()) {
-                      print("Login realizado...");
+                      //print("Login realizado...");
+                      fazerAutenticacao();
                       // * É aqui onde a programação para alterar o BD acontece
                     }
                   },
@@ -126,7 +225,8 @@ class _LoginViewState extends State<LoginView> {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) => const IsClienteFuncionarioGerenteView()),
+                            builder: (context) =>
+                                const IsClienteFuncionarioGerenteView()),
                       );
                     },
                     child: const Text(
